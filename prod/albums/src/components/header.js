db92645a7bda4b5493a87986ed2efa15c6473776
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-13 22:54:26 -0700

    Component

---
+++ prod/albums/src/components/header.js
@@ -0,0 +1,14 @@
+//import Component
+
+import React from 'react';
+import { Text } from 'react-native';
+
+//make a Component
+
+const Header = () => {
+  return <Text> Albums! </Text>;
+};
+
+
+//make Header Component available to root Component
+export default Header;
