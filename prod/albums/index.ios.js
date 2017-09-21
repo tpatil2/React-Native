59742af519bca69f8aa9d856a86a3d75fc1718c7
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-20 23:27:08 -0700

    AlbumList Component

--- prod/albums/index.ios.js
+++ prod/albums/index.ios.js
@@ -3,13 +3,17 @@
 
 //import library for Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 //create a Component with JSX
 
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 //render it on device
---
+++ prod/albums/src/components/AlbumList.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const AlbumList = () => {
+
+return (
+    <View>
+      <Text> AlbumList </Text>
+    </View>
+  );
+};
+
+export default AlbumList;
