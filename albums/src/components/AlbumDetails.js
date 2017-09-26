0e4ddc3262dc1e9755db75748fad64f0e576e8ad
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-25 22:11:45 -0700

    add image styling

--- albums/src/components/AlbumDetails.js
+++ albums/src/components/AlbumDetails.js
@@ -1,18 +1,46 @@
 
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) =>{
+
+  //multiple refrecences to props abject
+
+  const { title, artist, thumbnail_image} = album;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View >
+          <Image
+            style={styles.imageStyle}
+            source={{ uri: thumbnail_image }}
+           />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+
+
       </CardSection>
     </Card>
   );
+};
+
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  imageStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
