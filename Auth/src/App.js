1fab92634555ca8d78d21e11290703f3a1aab774
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-28 00:48:20 -0700

    Add firebase code

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { Text , View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header} from './component/common';
+import LoginForm from './component/LoginForm';
 
 class App extends Component {
 
+  componentwillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyAPumu2AapdbLusmnodO4BZi_O1qbZxC3Y",
+      authDomain: "authentication-ab2a6.firebaseapp.com",
+      databaseURL: "https://authentication-ab2a6.firebaseio.com",
+      projectId: "authentication-ab2a6",
+      storageBucket: "authentication-ab2a6.appspot.com",
+      messagingSenderId: "798916811371"
+    });
+  }
   render(){
     return(
       <View>
         <Header headerText="Authenticator" />
-        <Text> Hello..!!</Text>
+        <LoginForm />
       </View>
     );
   }
