2650a80aeac88d7e6ba7c101c4e1dcde87c639b3
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-27 22:41:43 -0700

    Import reusable components

---
+++ Auth/src/component/common/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPressprops, children }) => {
+
+  const { ButtonStyle, textStyle } = styles;
+  return (
+    <TouchableOpacity onPress={onPressprops} style={ButtonStyle} on>
+      <Text style={textStyle}>
+       {children}
+       </Text>
+    </TouchableOpacity>
+  );
+};
+
+
+const styles = {
+  ButtonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#999999',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#000000',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
+
+export default Button;
---
+++ Auth/src/component/common/CardSection.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+
+const styles = {
+    containerStyle: {
+      borderBottomWidth: 1,
+      padding: 5,
+      borderColor: '#000000',
+      backgroundColor: '#fff',
+    //  backgroundColor: '#bfbfbf',
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+      borderColor: '#ddd',
+      position: 'relative',
+    }
+
+};
+
+export default CardSection;
