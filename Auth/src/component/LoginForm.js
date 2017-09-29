59f467d04d479e2dd3869f618e9f57ef43172927
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-09-28 23:36:56 -0700

    Insert login username

--- Auth/src/component/LoginForm.js
+++ Auth/src/component/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card , CardSection } from './common';
+import { Button, Card , CardSection, Input } from './common';
 
 class LoginForm extends Component {
 
-  render(){
+  state = { email: ''};
+
+  render() {
       return(
         <Card>
-          <CardSection />
+          <CardSection>
+            <Input
+              label="Email"
+              placeholder="user@email.com"
+              value={this.state.email}
+              onChangeText={email => this.setState({ email })}
+            />
+          </CardSection>
 
           <CardSection />
 
--- Auth/src/component/common/index.js
+++ Auth/src/component/common/index.js
@@ -3,3 +3,4 @@ export * from './Button';
 export * from './Header';
 export * from './Card';
 export * from './CardSection';
+export * from './Input';
