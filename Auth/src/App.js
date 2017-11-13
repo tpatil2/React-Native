d2de96a83a732df19fcfd3a0e65f21620fe9d9e4
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-12 23:09:55 -0800

    onPress

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header} from './component/common';
+import { Header } from './component/common';
 import LoginForm from './component/LoginForm';
 
 class App extends Component {
 
-  componentwillMount(){
+  componentwillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyAPumu2AapdbLusmnodO4BZi_O1qbZxC3Y",
-      authDomain: "authentication-ab2a6.firebaseapp.com",
-      databaseURL: "https://authentication-ab2a6.firebaseio.com",
-      projectId: "authentication-ab2a6",
-      storageBucket: "authentication-ab2a6.appspot.com",
-      messagingSenderId: "798916811371"
+      apiKey: 'AIzaSyAPumu2AapdbLusmnodO4BZi_O1qbZxC3Y',
+      authDomain: 'authentication-ab2a6.firebaseapp.com',
+      databaseURL: 'https://authentication-ab2a6.firebaseio.com',
+      projectId: 'authentication-ab2a6',
+      storageBucket: 'authentication-ab2a6.appspot.com',
+      messagingSenderId: '798916811371'
     });
   }
-  render(){
-    return(
+  render() {
+    return (
       <View>
         <Header headerText="Authenticator" />
         <LoginForm />
--- Auth/src/component/LoginForm.js
+++ Auth/src/component/LoginForm.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import { Button, Card , CardSection, Input } from './common';
+import { Button, Card ,CardSection ,Input } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
 
-  state = { email: '', password: ''};
+  state = { email: '', password: '', error: '' };
+
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: 'Button pressed' });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication Failed' });
+        });
+      });
+    }
 
 
   render() {
-      return(
+      return (
         <Card>
           <CardSection>
             <Input
               label="Email"
-              secureTextEntry ={false}
+              secureTextEntry={false}
               placeholder="user@email.com"
               value={this.state.email}
               onChangeText={email => this.setState({ email })}
@@ -22,18 +38,20 @@ class LoginForm extends Component {
           <CardSection >
             <Input
               label="Password"
-              secureTextEntry={true}
+              secureTextEntry
               placeholder="*********"
               value={this.state.password}
               onChangeText={password => this.setState({ password })}
             />
-
           </CardSection>
 
+          <Text style={styles.errorTextStyle}>
+            {this.state.error}
+          </Text>
 
           <CardSection>
-            <Button>
-              Login
+            <Button onPress={this.onButtonPress.bind(this)} >
+              Log in
             </Button>
           </CardSection>
 
@@ -41,5 +59,14 @@ class LoginForm extends Component {
       );
   }
 }
+
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
