5bbe3b706a0e294f676029d181f60b8dc13003bc
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-13 21:13:33 -0800

    Refactering

--- Auth/src/component/LoginForm.js
+++ Auth/src/component/LoginForm.js
@@ -1,7 +1,7 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { Button, Card ,CardSection ,Input, Spinner } from './common';
 import { Text } from 'react-native';
-import firebase from 'firebase';
 
 class LoginForm extends Component {
 
@@ -12,12 +12,32 @@ class LoginForm extends Component {
     const { email, password } = this.state;
 
     this.setState({ error: '', loading: true });
+
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSucess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .catch(() => {
-          this.setState({ error: 'Authentication Failed' });
-        });
+        .then(this.onLoginSucess.bind(this))
+        .catch(this.onLoginFaild.bind(this))
+      });
+    }
+
+
+    onLoginFaild() {
+      this.setState({
+        error: 'Authentication Failed',
+        loading: false
+      });
+    }
+
+
+
+    onLoginSucess() {
+      this.setState({
+        email: '',
+        password: '',
+        loading: false,
+        error: ''
       });
     }
 
