6be1fe5903c7808e6b968c146f1d3ff53210721a
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-16 23:48:31 -0800

    Add list view

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import ListItem from './ListItem';
 
 
 class LibraryList extends Component {
 
-  render() {
-    console.log(this.props);
-    return;
+  // initialioze data source for ListView copied from official documentation
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
 
+  renderRow(library) {
+      return <ListItem library={library} />;
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
 }
 
 const mapStateToProps = state => {
