ebb2e001dfa775819ed64346ac0324b6c870782e
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-19 00:27:28 -0800

    added redux thunk to asynch action creater

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -2,6 +2,7 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
 } from './types';
+import firebase from 'firebase';
 
 export const emailChanged = (text) => {
   return {
@@ -15,5 +16,15 @@ export const passwordChanged = (text) => {
   return {
     type: PASSWORD_CHANGED,
     payload: text
+  };
+};
+
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: 'LOGIN_USER_SUCESS', payload: user });
+      });
   };
 };
--- manager/src/component/LoginForm.js
+++ manager/src/component/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
@@ -12,6 +12,11 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
    this.props.passwordChanged(text);
+ }
+
+ onButtonPress() {
+   const { email, password } = this.props;
+   this.props.loginUser({ email, password });
  }
 
   render() {
@@ -37,7 +42,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
@@ -53,4 +58,8 @@ const mapStateToProps = state => {
    };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged,
+  passwordChanged,
+  loginUser
+})(LoginForm);
