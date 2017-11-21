65a0b761965a9b0168f4a3865b5bee376c5fa3dc
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-20 21:53:38 -0800

    add nevigation using <scence>

---
+++ manager/src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './component/LoginForm';
+import EmployeeList from './component/EmployeeList';
+
+
+const RouterComponent = () => {
+  return (
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+      <Scene key="main">
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;
--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 
 import {
   EMAIL_CHANGED,
@@ -47,6 +49,8 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.main();
 };
 
 
