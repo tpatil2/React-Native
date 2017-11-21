bb39ca2302e8d19c80c4441a52259fe94d45bafd
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-11-20 22:47:33 -0800

    add employee form reducers

--- manager/src/component/EmployeeCreate.js
+++ manager/src/component/EmployeeCreate.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Button } from './common';
+
+
 
 class EmployeeCreate extends Component {
 
   render() {
       return (
-        <View>
-          <Text> EmployeeCreate </Text>
-          <Text> EmployeeCreate </Text>
-          <Text> EmployeeCreate </Text>
-          <Text> EmployeeCreate </Text>
-          <Text> EmployeeCreate </Text>
-        </View>
+        <Card>
+          <CardSection>
+            <Input
+
+            />
+          </CardSection>
+
+          <CardSection>
+            <Input
+
+            />
+          </CardSection>
+
+          <CardSection>
+            <Button
+            
+            />
+          </CardSection>
+        </Card>
       );
   }
 }
